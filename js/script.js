37d4b074d54597fb8feeb6114b33301ec1e61304
blackrose <wahomekelving@gmail.com>
2018-12-10 17:34:21 +0300

    Added the display function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $("form#form").submit(function(event){
-    var qN1 = $("input:radio[name=question1]:checked").val();
-    var qN2 = $("input:radio[name=question2]:checked").val();
-    var qN3 = $("input:radio[name=question3]:checked").val();
-    var qN4 = $("input:radio[name=question4]:checked").val();
-    var qN5 = $("input:radio[name=question5]:checked").val();
+  $("#submit").click(function(event) {
+    var questOne = $("input:radio[name=question1]:checked").val();
+    var questTwo = $("input:radio[name=question2]:checked").val();
+    var questThree = $("input:radio[name=question3]:checked").val();
+    var questFour = $("input:radio[name=question4]:checked").val();
+    var questFive = $("input:radio[name=question5]:checked").val();
 
-    var yourScore = parseInt(qN1)+parseInt(qN2)+parseInt(qN3)+parseInt(qN4)+parseInt(qN5);
-      $("#yourScore").text("Your score is:") + yourScore + "%");
+    var yourScore = parseInt(questOne) + parseInt(questTwo) + parseInt(questThree) + parseInt(questFour) + parseInt(questFive);
+    $("#yourScore").text("Your score is:" + yourScore + "%");
 
+    $("#formOne").hide();
+    $("#yourScore").show();
+    event.preventDefault();
+  });
 
-  })
-})
+});
